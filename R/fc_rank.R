30ca1628bec8c4374cf4fa30b45f01948919edf3
swhitCBR <swhit@uw.edu>
2021-10-07 15:39:04 -0700

    omitting censored values before computing GOF

--- R/fc_rank.R
+++ R/fc_rank.R
@@ -22,15 +22,23 @@ fc_rank <- function(x){
   surv_predDF$ucl=c(1,KM_mod$upper);surv_predDF$npars=NA
 
   fit_DF=x[["fit_vals"]]
-  # print(match(fit_DF$time,surv_predDF$time))
-  # print(str(surv_predDF$est[match(fit_DF$time,surv_predDF$time)]))
   # # fitted values and KM (nonparametric method)
   fit_DF$km_est=surv_predDF$est[match(fit_DF$time,surv_predDF$time)]
+  
+  # Checking for censored columns
+  fit_DF$non_cen=FALSE
+  fit_DF$non_cen[(fit_DF$time!=0)]=rep(x[["times"]]$non_cen,length(x$mod_choice))
+
+  # SUBSET to non-zeros and uncensored obs.
+  fit_DF=fit_DF[fit_DF$non_cen,]
+  
   # # # resid
   fit_DF$resid=fit_DF$est-fit_DF$km_est
   # # # squared difference
   fit_DF$sqr_err=(fit_DF$est-fit_DF$km_est)^2
 
+  
+  
   # summary table
   sumDF=data.frame(
     SSE_KM=tapply(fit_DF$sqr_err[-1],fit_DF$model[-1],sum),
@@ -46,9 +54,6 @@ fc_rank <- function(x){
   sumDF=sumDF[order(sumDF$GOF,decreasing = F),]
   sumDF$GOF=round(sumDF$GOF,4)
   rownames(sumDF)=NULL
-
-  # out=c(x,structure(list("GOF_tab"=sumDF),class="failmod_list"))
-  # out=c(x,structure(list("GOF_tab"=sumDF),class="failmod_list"))
 
   if(is.null(x$"GOF_tab")){
     out1=c(x,list("GOF_tab"=sumDF))
