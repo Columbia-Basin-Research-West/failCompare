bab56933e8fa5f143d4514da5cabbcbf723ab1b2
swhitCBR <swhit@uw.edu>
2021-12-13 14:27:04 -0800

    fixing quote issues and adding K-M objects as arguments

--- R/fc_fit_single.R
+++ R/fc_fit_single.R
@@ -6,19 +6,23 @@
 #' @param model failure time model
 #' @param Hess calculating standard errors
 #' @param non_cen logical of length(y)
+#' @param y_sfrac survival fraction
+#' @param  KM_DF kaplan-meier predictions
+#' @param  KM_mod kaplan-meier model object
 #' @param ... additional arguments passed to fc_tryfit()
 #'
 #' @return 
 #'
-fc_fit_single=function(y,model,Hess,non_cen,...){
+fc_fit_single=function(y,y_sfrac,model,Hess,non_cen,KM_DF,KM_mod,...){
   
   rc=ifelse(all(non_cen),FALSE,TRUE)
+  # y_cen=non_cen
   
   for (i in 1:length(model)){
     # FITTING DISTRIBUTIONS IN THE FLEXSURV PACKAGE
     if(model %in% names(fc_mod_ls)[names(fc_mod_ls) %in% names(flexsurv::flexsurv.dists)]){
       flex_mod=quote(model)
-      y_sfrac=failCompare::fc_surv(time = y,censorID = non_cen)
+      # y_sfrac=failCompare::fc_surv(time = y,censorID = non_cen)
       q_e=quote(flexsurv::flexsurvreg(survival::Surv(time=y,event=non_cen) ~ 1,
                                       dist = model,hessian = Hess))
       fit <- fc_tryfit(y = y,non_cen = non_cen,fit_call = q_e,model = model,Hess=Hess)
@@ -32,29 +36,38 @@ fc_fit_single=function(y,model,Hess,non_cen,...){
       if(model=="vitality.ku"){
         # Defines function call depending on right censoring or not
         if(rc){
-          dTmp=vitality::dataPrep(c(0,y_cen),(n_cen:(n_cen-length(y_cen)))/n_cen,datatype="CUM",rc.data=(n_cen>length(y_cen)))
-          y_sfrac=dTmp[,"sfract"]
-          q_e=vitality::vitality.ku(dTmp[,"time"],sdata = dTmp[,"sfract"],rc.data = T,pplot =F,silent=T,se=Hess,...)}
+          # dTmp=vitality::dataPrep(c(0,y_cen),(n_cen:(n_cen-length(y_cen)))/n_cen,datatype="CUM",rc.data=(n_cen>length(y_cen)))
+          # y_sfrac=dTmp[,"sfract"]
+          # q_e=vitality::vitality.ku(time=sort(y),sdata = y_sfrac,rc.data = T,pplot =F,silent=T,se=Hess)
+          q_e=quote(vitality::vitality.ku(time=sort(y),sdata = y_sfrac,rc.data = T,pplot =F,silent=T,se=Hess))
+        }
         else{
-          y_sfrac=failCompare:::fc_surv(y)
-          q_e=vitality::vitality.4p(time = sort(y),sdata = y_sfrac,pplot =F,silent = T,se=Hess)}
-        fit = fc_tryfit(fit_call = q_e,y = y,model="vitality.ku")
+          # y_sfrac=failCompare::fc_surv(y)
+          # q_e=vitality::vitality.ku(time = sort(y),sdata = y_sfrac,pplot =F,silent = T,se=Hess)
+          q_e=quote(vitality::vitality.ku(time = sort(y),sdata = y_sfrac,pplot =F,silent = T,se=Hess,rc.data=F))
+        }
+        fit = fc_tryfit(fit_call = q_e,y = y,y_sfrac=y_sfrac,model="vitality.ku",Hess=Hess)
+        # fit = fc_tryfit(fit_call = q_e,y = y,model="vitality.ku")
         pars_tmp=fit
         fit_vals=data.frame(model="vitality.ku",
                                   time=c(0,y),
                                   est=c(1,vitality::SurvFn.ku(y,pars_tmp[1],pars_tmp[2],pars_tmp[3],pars_tmp[4])),
-                                  lcl=0,ucl=0)}
-
+                                  lcl=0,ucl=0)
+      }
+      
+      
       if(model=="vitality.4p"){
         # Defines function call depending on right censoring or not
         if(rc){
-          dTmp=vitality::dataPrep(c(0,y_cen),(n_cen:(n_cen-length(y_cen)))/n_cen,datatype="CUM",rc.data=(n_cen>length(y_cen)))
-          y_sfrac=dTmp[,"sfract"]
-          q_e=vitality::vitality.4p(dTmp[,"time"],sdata = dTmp[,"sfract"],rc.data = T,pplot =F,silent=T,se=Hess,...)}
+          # dTmp=vitality::dataPrep(c(0,y_cen),(n_cen:(n_cen-length(y_cen)))/n_cen,datatype="CUM",rc.data=(n_cen>length(y_cen)))
+          # y_sfrac=dTmp[,"sfract"]
+          q_e=quote(vitality::vitality.4p(time = y,sdata = y_sfrac,pplot =F,silent = T,se = Hess))
+        }
         else{
-          y_sfrac=failCompare:::fc_surv(y)
-          q_e=vitality::vitality.4p(time = sort(y),sdata = y_sfrac,pplot =F,silent = T,se=Hess)}
-        fit = fc_tryfit(fit_call = q_e,y = y,model="vitality.4p")
+          # y_sfrac=failCompare::fc_surv(y)
+          q_e=quote(vitality::vitality.4p(time = y,sdata = y_sfrac,pplot =F,silent = T,se = Hess))
+          }
+        fit = fc_tryfit(fit_call = q_e,y = y,y_sfrac=y_sfrac,model="vitality.4p",Hess=Hess)
         pars_tmp=fit
         fit_vals=data.frame(model="vitality.4p",
                                   time=c(0,y),
@@ -62,9 +75,9 @@ fc_fit_single=function(y,model,Hess,non_cen,...){
                                   lcl=0,ucl=0)
       }
       if(model=="weibull3"){
-        y_sfrac=failCompare::fc_surv(y)
+        # y_sfrac=failCompare::fc_surv(y)
         q_e=quote(taglife.fn_weib3(y,model.in = "weibull",tag.se=eval(Hess)))
-        tmp=fc_tryfit(y=y,fit_call=q_e,model="weibull3",Hess = eval(Hess))
+        tmp=fc_tryfit(y=y,fit_call=q_e,model="weibull3",Hess = Hess)
         if(is.vector(tmp$par_tab)){Hess=F} # switch to FALSE if hessian caused error
         fit = tmp$mod_obj
         pars_tmp=tmp$par_tab
@@ -87,8 +100,8 @@ fc_fit_single=function(y,model,Hess,non_cen,...){
               "fit_vals"=fit_vals,
               "mod_objs"=fit,
               "par_tab"=par_tab,
-              # "KM_DF"=KM_DF,
-              # "KM_mod"=KM_mod,
+              "KM_DF"=KM_DF,
+              "KM_mod"=KM_mod,
               "censored"=rc)
   rownames(out_ls[["par_tab"]])=NULL
   out=structure(out_ls,class="fc_obj")
