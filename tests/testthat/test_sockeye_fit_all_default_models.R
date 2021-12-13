2f623276dd1ff783137e165f6704bd464d5fc8d2
swhitCBR <swhit@uw.edu>
2021-12-13 13:36:03 -0800

    added runs of sockeye data using fc_fit_single(), fc_tryfit() to test code

--- tests/testthat/test_sockeye_fit_all_default_models.R
+++ tests/testthat/test_sockeye_fit_all_default_models.R
@@ -1,3 +1,105 @@
+
+library(failCompare)
+
+data(sockeye)
+taglife=sockeye[,1]
+
+# model name vectors
+flex_mods=names(failCompare:::fc_mod_ls)[names(failCompare:::fc_mod_ls) %in% names(flexsurv::flexsurv.dists)]
+vit_mods=names(failCompare:::fc_mod_ls)[grep(names(failCompare:::fc_mod_ls),pattern="vitality")]
+all_mods=c(flex_mods,vit_mods,"weibull3")
+
+my_km=fc_fit(taglife,model = "kaplan-meier")
+my_km$KM_DF
+my_km$KM_mod
+
+########################################## #
+### Testing fc_fit_single() expresssions
+########################################## #
+
+# checking flexsurv mods
+lapply(flex_mods,function(modnms){
+  fc_fit_single(y = taglife,
+                y_sfrac=fc_surv(taglife),
+                model=modnms,
+                Hess=T,
+                non_cen = rep(TRUE,length(taglife)),
+                KM_DF=my_km$KM_DF,
+                KM_mod=my_km$KM_mod)})
+
+lapply(flex_mods,function(modnms){
+  fc_fit_single(y = taglife,
+                y_sfrac=fc_surv(taglife),
+                model=modnms,
+                Hess=F,
+                non_cen = rep(TRUE,length(taglife)),
+                KM_DF=my_km$KM_DF,
+                KM_mod=my_km$KM_mod)})
+
+
+# checking weibull3 model
+fc_fit_single(y = taglife,
+              y_sfrac=fc_surv(taglife),
+              model="weibull3",
+              Hess=T,
+              non_cen = rep(TRUE,length(taglife)),
+              KM_DF=my_km$KM_DF,
+              KM_mod=my_km$KM_mod)
+
+fc_fit_single(y = taglife,
+              y_sfrac=fc_surv(taglife),
+              model="weibull3",
+              Hess=F,
+              non_cen = rep(TRUE,length(taglife)),
+              KM_DF=my_km$KM_DF,
+              KM_mod=my_km$KM_mod)
+
+
+### vitality mods
+lapply(vit_mods,function(modnms){
+  fc_fit_single(y = taglife,
+                y_sfrac=fc_surv(taglife),
+                model=modnms,
+                Hess=T,
+                non_cen = rep(TRUE,length(taglife)),
+                KM_DF=my_km$KM_DF,
+                KM_mod=my_km$KM_mod)})
+
+
+##################################### #
+### Testing fc_tryfit() expresssions
+##################################### #
+
+## Directly use these
+
+flx_mds=list()
+for(i in 1:length(flex_mods)){
+  flx_mds[[i]]=quote(flexsurv::flexsurvreg(survival::Surv(time=taglife,event=rep(TRUE,length(taglife))) ~ 1,
+                                           dist = eval(flex_mods[i]),hessian = T))}
+
+# funcion calls evaluated by fc_tryfit()
+test_e=c(
+  flx_mds,
+  quote(vitality::vitality.ku(taglife,sdata = fc_surv(taglife),rc.data = F,pplot =F,silent=T,se=T)),
+  quote(vitality::vitality.4p(taglife,sdata = fc_surv(taglife),rc.data = F,pplot =F,silent=T,se=T)),
+  quote(taglife.fn_weib3(taglife,model.in = "weibull",tag.se=T))
+)
+
+# evaluating function calls
+lapply(1:9,function(x){fc_tryfit(fit_call = eval(test_e[[x]]),model = all_mods[x],Hess=T)})
+
+
+
+##################################### #
+### Testing fc_fit() expresssions
+##################################### #
+
+
+
+
+
+
+
 # library(testthat)
 data(sockeye)
 taglife=sockeye[,1]
